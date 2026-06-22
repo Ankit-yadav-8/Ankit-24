a24d14b57c752db77473f8f3133fda8a75b44f26
Ankit-IITR <ankityadav08022008@gmail.com>
2026-06-22 11:20:00 +0530

    fix: update gitignore, email sender to hello@collegeparichay.in, brevo domain auth

--- 24K Media/client/vite.config.js
+++ 24K Media/client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ankit-24/',
+  base: '/',  // ✅ Correct for Vercel,
   server: {
     port: 5173,
     proxy: {
